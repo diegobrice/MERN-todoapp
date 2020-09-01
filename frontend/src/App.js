cc6119da4c6fa198883eab6c4aa3929dd2946b3b
Diego Briceño <diegobrice@hotmail.com>
2020-09-01 00:22:45 -0500

    added username component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,10 +12,12 @@ function App() {
     <div className="App">
       <Router>
         <Navigation />
-        <Route path="/" exact component={NotesList} />
-        <Route path="/edit/:id" component={CreateNote} />
-        <Route path="/create" component={CreateNote} />
-        <Route path="/user" component={CreateUser} />
+        <div className="container p-4">
+          <Route path="/" exact component={NotesList} />
+          <Route path="/edit/:id" component={CreateNote} />
+          <Route path="/create" component={CreateNote} />
+          <Route path="/user" component={CreateUser} />
+        </div>
       </Router>
     </div>
   );
--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -1,9 +1,75 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class CreateUser extends Component {
-  state = {};
+  state = {
+    users: [],
+    username: "",
+  };
+  async componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers = async () => {
+    const res = await axios.get("http://localhost:4000/api/users");
+    this.setState({ users: res.data });
+  };
+
+  onChangeUsername = (e) => {
+    this.setState({ username: e.target.value });
+  };
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post("http://localhost:4000/api/users", {
+      username: this.state.username,
+    });
+    console.log(res);
+    this.setState({ username: "" });
+    this.getUsers();
+  };
+
+  deleteUser = async (id) => {
+    await axios.delete(`http://localhost:4000/api/users/${id}`);
+    this.getUsers();
+  };
+
   render() {
-    return <h2>CreateUser</h2>;
+    return (
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card card-body">
+            <h3>Crear nuevo usuario</h3>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  value={this.state.username}
+                  onChange={this.onChangeUsername}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Guardar
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="col-md-8">
+          <ul className="list-group">
+            {this.state.users.map((user) => (
+              <li
+                key={user._id}
+                className="list-group-item list-group-item-action"
+                onDoubleClick={() => this.deleteUser(user._id)}
+              >
+                {user.username}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
